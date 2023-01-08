97052f846bca5d2ae6283007a2917ce8ba2c050f
klxex-admin <hwanguebest@gmail.com>
2023-01-08 15:00:42 +0900

    init: 초기화

--- src/main/java/com/hwan/websocket/WebsocketApplication.java
+++ src/main/java/com/hwan/websocket/WebsocketApplication.java
@@ -9,6 +9,7 @@ public class WebsocketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketApplication.class, args);
 		System.out.println("hello world 입니다");
+		System.out.println("github에서 update를 했습니다");
 	}
 
 }
